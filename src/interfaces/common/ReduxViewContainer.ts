ee1dcb626b1bccfb5d5aa31f05d4667350213dc1
Denghejun <hjdeng@CNhjdeng.local>
2017-12-20 22:54:58 +0800

    extend reduxt view container base

--- src/interfaces/common/ReduxViewContainer.ts
+++ src/interfaces/common/ReduxViewContainer.ts
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { connect, InferableComponentEnhancer, Dispatch, DispatchProp, Component } from 'react-redux'
-import { injectable } from 'inversify'
+import { injectable, Container } from 'inversify'
 
 @injectable()
 export abstract class ReduxViewContainer<TView extends React.Component> {
@@ -13,5 +13,27 @@ export abstract class ReduxViewContainer<TView extends React.Component> {
   protected abstract MapDispatchToProps(dispatch: Dispatch<any>, ownProps: any): any;
   public create() {
     return connect(this.MapStateToProps.bind(this), this.MapDispatchToProps.bind(this))(this.View);
+  }
+
+  private static registerReduxContainerConnected(container: Container,
+    originalReduxContainerConnectedType: symbol,
+    registerReduxContainerConnectedType: symbol): void {
+    container.bind(registerReduxContainerConnectedType).toDynamicValue(context => {
+      return context.container.get<ReduxViewContainer<any>>(originalReduxContainerConnectedType).create();
+    });
+  }
+
+  private static registerOriginalReduxContainer(container: Container,
+    originalReduxContainerConnectedType: symbol,
+    reduxViewContainer: new (...args: any[]) => ReduxViewContainer<any>): void {
+    container.bind<ReduxViewContainer<any>>(originalReduxContainerConnectedType).to(reduxViewContainer);
+  }
+
+  public static registerReduxViewContainer(container: Container,
+    reduxViewContainer: new (...args: any[]) => ReduxViewContainer<any>,
+    originalReduxContainerConnectedType: symbol,
+    registerReduxContainerConnectedType: symbol): void {
+    this.registerOriginalReduxContainer(container, originalReduxContainerConnectedType, reduxViewContainer);
+    this.registerReduxContainerConnected(container, originalReduxContainerConnectedType, registerReduxContainerConnectedType);
   }
 }
